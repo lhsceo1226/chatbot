cd07a8fa7765bdc581cf2cccc18c35517e34003f
bit\bitcamp <lhsceo1226@hanmail.net>
2017-12-04 19:17:23 +0900

    js
    js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText + : "Seems like some problem. Speak again."
-    var speech = "바보" ? "멍청이" + : "Seems like some problem. Speak again."
+    var speech = "바보" && req.body.result.parameters.echoText ? "멍청이" + : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
